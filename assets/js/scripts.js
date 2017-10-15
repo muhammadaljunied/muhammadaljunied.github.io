1279caecef8de37c60db89b224c9c0eff4632915
Muhammad Aljunied <muhammadaljunied@gmail.com>
2017-10-15 23:08:23 +0800

    Updated Personal Statement, Resume, Skills, Contact and Locaton

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -277,7 +277,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
+        var myLatlng = new google.maps.LatLng(1.3674374, 103.9514448);
 
             var styles = [
                 {
